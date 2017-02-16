c444406bfa9ab85ad4edc65713c5845c08cf5499
Michael Pimlott <pettingbutterscotch@gmail.com>
2017-02-16 13:51:57 -0800

    add style.css

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ var express = require("express");
 /*initilaize express app object*/
 
 var app = express();
+
+/* variables */
+var root = __dirname
+  , port = 3000;
+
+/*configure express app*/
+
+app.use( express.static(__dirname + "/public"));
+
+/* start server on port 3000 */
+
+app.listen( port, function listenCallback() {
+  console.log("Express server is listening on port " + port);
+  console.log("To test, browse to http://localhost:" + port);
+});
