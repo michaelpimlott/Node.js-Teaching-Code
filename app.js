fda3a36086d88d939210059134f83fdb548ea500
Michael Pimlott <pettingbutterscotch@gmail.com>
2017-02-16 14:29:13 -0800

    add HTML and JSON routes

--- app.js
+++ app.js
@@ -14,6 +14,31 @@ var root = __dirname
 
 app.use( express.static(__dirname + "/public"));
 
+/* create express routes */
+
+app.get( '/hello', function helloCallback(req, res) {
+  res.send("Hello, there handsome!");
+});
+
+/*sending JSON*/
+
+app.get( "/someJSON", function someJSONCallback(req, res) {
+  res.json({
+    "one": {
+      "so":"cool"
+    },
+    "two":"super cool",
+    "three": ["love", "JSON", "and", "JavaScript"]
+  });
+});
+
+/*basic html string*/
+
+app.get( "/begin", function beginCallback(req, res) {
+
+  res.send( "<h1>This is just the start</h1><p><3</p>")
+})
+
 /* start server on port 3000 */
 
 app.listen( port, function listenCallback() {
